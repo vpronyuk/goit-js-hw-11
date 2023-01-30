837057e714a611b434aa867d4785faacd09eaccc
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-01-30 16:47:18 +0200

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateTextarea();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onTextareaInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea(evt) {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    document.querySelector('textarea').value = savedFormData.message;
+    document.querySelector('input').value = savedFormData.email;
+  }
+}
+
+function populateInput(evt) {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData) {
+    document.querySelector('input').value = savedFormData.email;
+  }
+}
