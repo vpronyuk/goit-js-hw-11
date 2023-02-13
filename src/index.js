6e58f9a04690426416a066404d2e0a599818c6e3
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-02-14 00:06:30 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import fetchPixabayAPI from './js/script';
+
+const form = document.getElementById('search-form');
+console.log('hello');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const form = evt.currentTarget;
+  const inputValue = form.elements.searchQuery.value;
+  console.log(inputValue);
+  fetchPixabayAPI(inputValue).then(data => {
+    console.log(data.json());
+  });
+}
